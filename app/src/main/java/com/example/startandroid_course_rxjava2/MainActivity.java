5a73756b277fc8626bdaa0a330ed032de3056678
buchgit <buchgit@gmail.com>
2021-03-22 22:39:24 +0300

    Observable.OnSubscribe

--- app/src/main/java/com/example/startandroid_course_rxjava2/MainActivity.java
+++ app/src/main/java/com/example/startandroid_course_rxjava2/MainActivity.java
@@ -6,6 +6,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import rx.Observable;
 import rx.Observer;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 import java.util.concurrent.TimeUnit;
 
@@ -18,7 +21,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Observable<Long> observable = Observable.interval(500,TimeUnit.MILLISECONDS);
+        Observable.OnSubscribe onSubscribe = new Observable.OnSubscribe<Integer>() {
+            @Override
+            public void call(Subscriber<? super Integer> subscriber) {
+                for (int i = 0; i < 10; i++) {
+                    try {
+                        TimeUnit.MILLISECONDS.sleep(300);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    Log.d(TAG,Thread.currentThread().getName());
+                    subscriber.onNext(i);
+                }
+                subscriber.onCompleted();
+            }
+        };
+
+        Observable<Long>observable = Observable
+                .create(onSubscribe)
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread());
 
         Observer<Long> observer = new Observer() {
             @Override
