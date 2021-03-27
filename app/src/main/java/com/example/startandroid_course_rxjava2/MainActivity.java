822eb72f8497187673a0c257c520057d44859be2
buchgit <buchgit@gmail.com>
2021-03-27 14:13:08 +0300

    retry(new Function2<Integer,Throwable,Boolean>) входит счетчик, ошибка, выходит булеан
    обработка ретри с возможностью добавить свой код

--- app/src/main/java/com/example/startandroid_course_rxjava2/MainActivity.java
+++ app/src/main/java/com/example/startandroid_course_rxjava2/MainActivity.java
@@ -10,6 +10,7 @@ import rx.Subscriber;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Action1;
 import rx.functions.Func1;
+import rx.functions.Func2;
 import rx.schedulers.Schedulers;
 
 import java.util.concurrent.TimeUnit;
@@ -19,6 +20,7 @@ onErrorReturn
 onErrorResumeNext
 onExceptionResumeNext не поймает как onErrorResumeNext ошибки Throwable и Error, но только Exception
 retry()
+retry(new Func2...)
  */
 
 public class MainActivity extends AppCompatActivity {
@@ -36,25 +38,41 @@ public class MainActivity extends AppCompatActivity {
                 .map(Long::parseLong)
 //                .onErrorReturn(throwable -> {   //перехватили ошибку и вернули 0L.
 //                                                // В зависимости от ошибки можно возвращать разные значения
-//                    Log.d(TAG, throwable.getMessage());
+//                    Log.d(TAG, "onErrorReturn "+ throwable.getMessage());
 //                    return 0L;//здесь будет вызван onCompleted
-//                });
+//                })
                 //.onErrorResumeNext(Observable.just(5L, 10L, 15L));  //можно вернуть другой Observable,
                                                                     // то есть несколько значений
 
-            .onErrorResumeNext(new Func1<Throwable, Observable<? extends Long>>() { //еще одна реализация
-                                                                                    // этого метода
+//            .onErrorResumeNext(new Func1<Throwable, Observable<? extends Long>>() { //еще одна реализация
+//                                                                                    // этого метода
+//                @Override
+//                public Observable<? extends Long> call(Throwable throwable) {
+//                    Log.d(TAG,"onErrorResumeNext " + throwable);
+//                    return Observable.just(8L, 9L, 10L);
+//                }
+//            })
+            //.retry(3);//переподписка на обсервебл возможна, если что-то пошло не так, 3-количество переподписок
+            .retry(new Func2<Integer, Throwable, Boolean>() {   //ограничиваем число повторов
+                                                                // и имеем возможность код исполнить какой-нибудь
+                                                                //после 3 попытки получит ошибку в onError
                 @Override
-                public Observable<? extends Long> call(Throwable throwable) {
-                    Log.d(TAG,"onErrorResumeNext " + throwable);
-                    return Observable.just(8L, 9L, 10L);
+                public Boolean call(Integer integer, Throwable throwable) {
+                    //Log.d(TAG, "call: "+ throwable.getMessage());
+                    return integer<3;
                 }
-            })
-            .retry(3);//переподписка на обсервебл возможна, если что-то пошло не так, 3-количество переподписок
+            });
 
-//        observable.subscribe(aLong -> Log.d(TAG, "call: " + aLong)
-//                , throwable -> Log.d(TAG, throwable.getMessage()));
-        observable.subscribe(aLong->Log.d(TAG,"call: " + aLong));//вывод на onNext
+        observable.subscribe(aLong -> Log.d(TAG, "call: " + aLong)
+                , throwable -> Log.d(TAG, "in observer: " + throwable.getMessage()));
+
+        //вариант ниже НЕ РАБОТАЕТ
+//      observable.subscribe(new Action1<Long>() {
+//            @Override
+//            public void call(Long aLong) {
+//                Log.d(TAG,"onNext "+ aLong);
+//            }
+//        },throwable -> Log.d("TAG","onError in observer " + throwable.getMessage()));//вывод на onNext
 
     }
 }
