877bcbfc722c6fe221a40926d1f8bad96fdff131
buchgit <buchgit@gmail.com>
2021-03-28 12:21:00 +0300

    Retrofit with RxJava

--- app/src/main/java/com/example/startandroid_course_rxjava2/MainActivity.java
+++ app/src/main/java/com/example/startandroid_course_rxjava2/MainActivity.java
@@ -8,6 +8,7 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 import rx.Observable;
 import rx.Observer;
@@ -39,6 +40,8 @@ compile 'io.reactivex:rxandroid:1.2.1'
 Вторая - способность Retrofit конвертировать json в объекты.
 Третья - способность Retrofit работать с RxJava.
 Четвертая и пятая строки - это RxJava
+
+observable.toBlocking() - синхронный запрос
  */
 
 public class MainActivity extends AppCompatActivity {
@@ -52,29 +55,45 @@ public class MainActivity extends AppCompatActivity {
 
         Retrofit retrofit = new Retrofit.Builder()
                 .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
                 .baseUrl("https://rawgit.com/startandroid/data/master/messages/")
                 .build();
 
         WebApi webApi = retrofit.create(WebApi.class);
 
-        Call<List<Message>> call = webApi.messages(2);
+        Observable<List<Message>> observable = webApi.messages(2);
 
-        call.enqueue(new Callback<List<Message>>() {
-            @Override
-            public void onResponse(Call<List<Message>> call, Response<List<Message>> response) {
-                if (response.isSuccessful()){
-                    assert response.body()!=null;
-                    Log.d(TAG, "onResponse, message count: " + response.body().size());
-                }else{
-                    Log.d(TAG, "onResponse error: "+ response.code());
-                }
-            }
+        observable.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<List<Message>>() {
+                    @Override
+                    public void onCompleted() {
+                        Log.d(TAG, "onCompleted: ");
+                    }
 
-            @Override
-            public void onFailure(Call<List<Message>> call, Throwable t) {
-                Log.d(TAG, "onFailure: "+ t.getMessage());
-            }
-        });
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.d(TAG, "onError: ");
+                    }
+
+                    @Override
+                    public void onNext(List<Message> messages) {
+                        Log.d(TAG, "onNext: size "+ messages.size());
+                    }
+                });
     }
 }
 
+/*
+Синхронный запуск
+Если вы не в UI-потоке и вам необходимо выполнить запрос синхронно, можно сделать так:
+
+List<Message> list = null;
+try {
+    list = observable.toBlocking().first();
+} catch (Exception e) {
+    e.printStackTrace();
+}
+
+Метод toBlocking сделает Observable синхронным, а first вернет первый элемент
+ */
--- app/src/main/java/com/example/startandroid_course_rxjava2/WebApi.java
+++ app/src/main/java/com/example/startandroid_course_rxjava2/WebApi.java
@@ -3,10 +3,12 @@ package com.example.startandroid_course_rxjava2;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
+import rx.Observable;
 
 import java.util.List;
 
+
 public interface WebApi {
     @GET("messages{page}.json")
-    Call<List<Message>> messages(@Path("page") int page);
+    Observable<List<Message>> messages(@Path("page") int page);
 }
