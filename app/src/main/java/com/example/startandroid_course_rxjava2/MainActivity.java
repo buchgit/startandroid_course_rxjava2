2c68b6b11bd4ad03f1461923b00844af44dd4394
buchgit <buchgit@gmail.com>
2021-03-29 17:01:36 +0300

    Observable и Flowable, Single, Completable, Maybe Изменения в Action and Function

--- app/src/main/java/com/example/startandroid_course_rxjava2/MainActivity.java
+++ app/src/main/java/com/example/startandroid_course_rxjava2/MainActivity.java
@@ -1,44 +1,119 @@
 package com.example.startandroid_course_rxjava2;
 
-import android.util.Log;
-import androidx.annotation.LongDef;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
-import retrofit2.converter.gson.GsonConverterFactory;
 import rx.Observable;
-import rx.Observer;
 import rx.Subscriber;
-import rx.android.schedulers.AndroidSchedulers;
-import rx.functions.Action1;
-import rx.functions.Func0;
-import rx.functions.Func1;
-import rx.functions.Func2;
-import rx.observables.SyncOnSubscribe;
-import rx.schedulers.Schedulers;
-
-import java.io.IOException;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
+import rx.Subscription;
 
 /*
-Урок 10. How to request data from Observable
+Урок 11. RxJava 2
 
-использовать
-Subscriber.class
+Источников данных в RxJava :
+Observable и Flowable, Single, Completable, Maybe
 
-new Subscriber<Integer>() {
+Observable не поддерживает backpressure
+
+Action1 переименован в Consumer, а Action0 в Action
+
+Используйте Observable, если
+1. Мало данных и, накапливаясь, они не смогут привести к OutOfMemoryError.
+
+2. Вы работаете с данными, которые не поддерживают backpressure, например:
+touch-события.
+
+Используйте Flowable, если
+1. Данные поддерживают backpressure, например: чтение с диска.
+
+//***************************** Observable ****************
+
+public interface Observer<T> {
+    void onSubscribe(@NonNull Disposable d);//новый метод в RxJava2
+    void onNext(@NonNull T t);
+    void onError(@NonNull Throwable e);
+    void onComplete();
+}
+public interface Disposable { //новый тип объекта
+    void dispose();
+    boolean isDisposed();
+}
+
+//новый тип объекта (обертка) вместо(для) Observer
+DisposableObserver disposableObserver = new DisposableObserver<Integer>() {
     @Override
-    public void onStart() {
-        super.onStart();
-        request(1);
+    public void onNext(@NonNull Integer integer) {
+
     }
 
+    @Override
+    public void onError(@NonNull Throwable e) {
+
+    }
+
+    @Override
+    public void onComplete() {
+
+    }
+};
+
+//***************************** Flowable ****************
+public interface Subscriber<T> {
+    public void onSubscribe(Subscription s);
+    public void onNext(T t);
+    public void onError(Throwable t);
+    public void onComplete();
+}
+
+public interface Subscription {
+    public void request(long n);
+    public void cancel();
+}
+
+
+//********************* Single *************************************
+Этот источник предоставляет либо один элемент в onNext, либо ошибку
+в onError, после чего передача данных будет считаться завершенной.
+onComplete не возвращает
+
+подписчик:
+public interface SingleObserver  {
+    void onSubscribe(@NonNull Disposable d);
+    void onSuccess(@NonNull Long aLong);
+    void onError(@NonNull Throwable e);
+}
+
+
+//********************* Completable *************************************
+Этот источник предоставит вам либо onComplete, либо ошибку в onError. Никаких данных в onNext он не передаст
+
+//********************* Maybe *************************************
+Его можно описать как Single OR Completable. Т.е. вам придет либо одно значение в onNext,
+ либо onCompleted, но не оба. Также может прийти onError
+
+Для Maybe, аналогично с Single и Completable, есть специальные подписчики:
+MaybeObserver - аналог обычного Observer, но с методом onSuccess вместо onNext
+DisposableMaybeObserver - аналог DisposableObserver, но с методом onSuccess вместо onNext
+
+//******************* Action, Function ***********************
+Action, Function
+
+Cписок, как были переименованы (или удалены) Action и Function
+
+Action0 -> Action
+Action1 -> Consumer
+Action2 -> BiConsumer
+ActionN -> Consumer<Object[]>
+Action3 - Action9 -> удалены
+
+Func0 -> Callable
+Func1 -> Function
+Func2 -> BiFunction
+Func3 - Func9 -> Function3 - Function9
+
  */
+
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -49,33 +124,66 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Observable.range(1, 10)
-                .subscribe(new Subscriber<Integer>() {
-                    @Override
-                    public void onStart() {
-                        super.onStart();
-                        request(1);//количество запрашиваемых данных
-                    }
+        Flowable<Integer> flowable = Flowable.range(1, 10);
 
-                    @Override
-                    public void onCompleted() {
-                        Log.d(TAG,"onCompleted");
-                    }
+        flowable.subscribe(new Subscriber<Integer>() {//Subscriber - нужно делать request
+                                                        //в onSubscribe
+            @Override
+            public void onSubscribe(Subscription s) {
+                s.request(Long.MAX_VALUE);  //нужно делать запрос, иначе данные не будут
+                                            //отправляться из Flowable
+            }
 
-                    @Override
-                    public void onError(Throwable e) {
-                        Log.d(TAG,"onError " + e);
-                    }
+            @Override
+            public void onNext(Integer integer) {
 
-                    @Override
-                    public void onNext(Integer integer) {
-                        Log.d(TAG,"onNext " + integer);
-                        request(1);//запрос новых данных после каждого получения
-                        //таким образом можно выбрать все данные, не указывая их количество
-                        //в методе onStart()
-                    }
-                });
+            }
 
+            @Override
+            public void onError(Throwable t) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        });
+
+        //Если использовать DisposableSubscriber вместо Subscriber , не нужен request
+        Disposable disposable = flowable.subscribeWith(new DisposableSubscriber<Integer>() {
+            @Override
+            public void onNext(Integer integer) {
+            }
+            @Override
+            public void onError(Throwable t) {
+            }
+            @Override
+            public void onComplete() {
+            }
+        });
+        // ...
+        disposable.dispose();
+
+        //Single
+
+        //для Single свой тип подписчика SingleObserver
+        single.subscribe(new SingleObserver<Long>() {
+            @Override
+            public void onSubscribe(@NonNull Disposable d) {
+
+            }
+
+            @Override
+            public void onSuccess(@NonNull Long aLong) {
+
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+
+            }
+        });
 
     }
 }
